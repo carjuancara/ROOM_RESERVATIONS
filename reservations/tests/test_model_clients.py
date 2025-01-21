b4208a1cdc7d8741aff6590ce7fe384a9587f6c0
Carlos Carabajal <carjuancara@gmail.com>
2025-01-21 10:25:25 -0300

    test(TESTS): :white_check_mark: validates the relationship between the models
    Create a record for each model to test the relationships between clients and reservations

--- reservations/tests/test_model_clients.py
+++ reservations/tests/test_model_clients.py
@@ -1,6 +1,6 @@
-from reservations.models import Clients
+from reservations.models import Clients, Reservation, Room
 from django.core.exceptions import ValidationError
-
+from datetime import datetime
 
 import pytest
 
@@ -104,3 +104,57 @@ class TestClients:
             value = getattr(new_client, field, None)
             assert value, f"El campo '{
                 field}' está vacío o no tiene un valor válido"
+
+    def test_client_reservation_relation(self):
+        # Creamos un cliente
+        client = Clients.objects.create(
+            name="Juan",
+            lastname="Perez",
+            email="juan.perez@example.com",
+            document_number="1234567890",
+            street="Calle Falsa 123",
+            city="Madrid",
+            state="Madrid",
+            country="España",
+            phone="+34600123456"
+        )
+
+        # creamos una habitacion para los test
+        room = Room.objects.create(
+            number=1,
+            type='single',
+            price_for_night=100.24,
+            is_reserved=False,
+            status='available',
+            description='descripcion de la habitacion',
+            capacity=4,
+            amenities={'wifi': True, 'air_conditioning': True, 'minibar': False,
+                       'jacuzzi': False, 'tv': True, 'breakfast_included': True}
+
+        )
+
+        # Creamos una reserva para ese cliente
+        reservation = Reservation.objects.create(
+            date_in="2025-01-01",
+            date_out="2025-01-05",
+            status="pending",
+            total_price=500.00,
+            client=client,  # Relación con el cliente
+            room_id=1,  # Suponiendo que ya tienes una habitación con ID 1
+            created_at=datetime.now(),  # Asignar el valor de la fecha de creación
+            updated_at=datetime.now(),
+        )
+
+        # Verificamos que la reserva está asociada correctamente con el cliente
+        # La relación debe ser la misma instancia de cliente
+        assert reservation.client == client
+
+        # También podemos verificar que el cliente tiene reservas asociadas
+        # Usamos el related_name 'reservations' para acceder a las reservas del cliente
+        assert reservation in client.reservations.all()
+
+        # Aseguramos que los datos de la reserva son correctos
+        assert reservation.date_in == "2025-01-01"
+        assert reservation.date_out == "2025-01-05"
+        assert reservation.status == "pending"
+        assert reservation.total_price == 500.00
