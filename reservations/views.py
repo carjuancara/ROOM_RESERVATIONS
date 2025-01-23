0830128a893039650605ff34f5f1dc171f6b4f3f
Carlos Carabajal <carjuancara@gmail.com>
2025-01-23 09:48:24 -0300

    feat: :sparkles: add viewsets
    ClientViewSet, RoomViewSet, ReservationViewSet

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Clients, Room, Reservation
+from .serializers import ClientSerializer, RoomSerializer, ReservationSerializer
 
-# Create your views here.
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Clients.objects.all()
+    serializer_class = ClientSerializer
+
+
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
