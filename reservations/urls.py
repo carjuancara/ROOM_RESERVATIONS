aca8fc8a59a3274914d2e027d3bd2b9b721ed2e1
Carlos Carabajal <carjuancara@gmail.com>
2025-01-24 08:41:18 -0300

    chore: :necktie: Adds the registration of views
    client, room and reservation views

--- reservations/urls.py
+++ reservations/urls.py
@@ -2,10 +2,11 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from .views import ClientViewSet, RoomViewSet, ReservationViewSet
 
+app_name = "reservations"
 router = DefaultRouter()
-router.register(r'client', ClientViewSet)
-router.register(r'room', RoomViewSet)
-router.register(r'reservation', ReservationViewSet)
+router.register(r'client', ClientViewSet, basename="client")
+router.register(r'room', RoomViewSet, basename="room")
+router.register(r'reservation', ReservationViewSet, basename="reservation")
 
 urlpatterns = [
     path('', include(router.urls))
