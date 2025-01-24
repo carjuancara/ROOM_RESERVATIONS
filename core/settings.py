c8cb4951d57eb7e1610efa36c0f79010f8f55764
Carlos Carabajal <carjuancara@gmail.com>
2025-01-24 12:00:27 -0300

    feat: :sparkles: add documentation api

--- core/settings.py
+++ core/settings.py
@@ -39,8 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    'drf_spectacular',
     'reservations',
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
