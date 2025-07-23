73fa7b4f23abd053b3ca86da53e444f984020734
Carlos J Carabajal <carjuancara@gmail.com>
2025-07-23 17:30:03 -0300

    chore: add routes

--- reservations/urls.py
+++ reservations/urls.py
@@ -10,5 +10,9 @@ router.register(r'reservation', ReservationViewSet, basename="reservation")
 router.register(r'register', RegisterUser, basename="register")
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('reservation/my_reservations/', ReservationViewSet.as_view(
+        {'get': 'my_reservations'}), name='reservation-my_reservations'),
+    path('room/availability/', RoomViewSet.as_view(
+        {'get': 'availability'}), name='room-availability')
 ]
